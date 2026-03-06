cd72f467a1d86c8f7078e5d66eaf4cdcd956decb
mguy <mguy@vbox.42lyon.fr>
2026-03-06 20:14:38 +0100

    add: Readme for p3 / edit install.sh - install.sh show now ArgoCD's authentification datas

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 # ==============================================================================
-# Script to install Docker, K3d, Kubectl and setup the cluster
+# Inception of Things - Part 3: K3d and Argo CD Setup
 # ==============================================================================
 
 echo "Updating system and installing prerequisites..."
@@ -10,7 +9,7 @@ sudo apt-get install -y curl git docker.io
 
 echo "Configuring Docker..."
 sudo systemctl enable --now docker
-sudo usermod -aG docker $USER  # according rights to user => sudi is not longer required
+sudo usermod -aG docker $USER
 
 echo "Installing K3d..."
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
@@ -20,27 +19,42 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
-# Give Docker a moment to start properly
 sleep 3
 
 echo "Creating K3d cluster (iot-cluster)..."
-# Port 8888 for the app, Port 8080 for Argo CD UI
 sudo k3d cluster create iot-cluster --port "8888:8888@loadbalancer" --port "8080:80@loadbalancer" --wait
 
-# Copy the kubeconfig so the standard user can use kubectl without sudo
 mkdir -p ~/.kube
-sudo k3d kubeconfig get iot-cluster > ~/.kube/config  # generate config file (API server address, certificates, and tokens needed to authenticate)
+sudo k3d kubeconfig get iot-cluster > ~/.kube/config
 sudo chown $USER:$USER ~/.kube/config
 
-echo "Creating namespaces (argocd and dev)..."
+echo "Creating namespaces..."
 kubectl create namespace argocd
 kubectl create namespace dev
 
 echo "Installing Argo CD..."
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+echo "Waiting for Argo CD CRDs to initialize (15s)..."
+sleep 15
+
+echo "Linking Argo CD to GitHub repository..."
+kubectl apply -f ../confs/application.yaml
+
+echo "Waiting for Argo CD Secret to be generated (10s)..."
+sleep 10
+
+while ! kubectl -n argocd get secret argocd-initial-admin-secret &>/dev/null; do
+    sleep 5
+done
+ARGOPWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
 echo "======================================================================="
 echo "Installation complete!"
-echo "IMPORTANT: Please log out and log back in (or type 'newgrp docker') "
-echo "to apply Docker group permissions without needing sudo."
+echo "ArgoCD User : admin"
+echo "ArgoCD Password : $ARGOPWD"
+echo ""
+echo "Access :"
+echo "   1. run : kubectl port-forward svc/argocd-server -n argocd 8443:443 &"
+echo "   2. Go to : https://localhost:8443"
 echo "======================================================================="
